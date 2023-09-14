1ef61d3ec5e3cc3abe8c23d7591499d4bbe539ab
BrunoDemartini <bruno.r.demartini@gmail.com>
2023-09-14 09:05:32 -0300

    Adicionado criptografica com bcrypt nas senhas do usuario e validação de senha incorreta

--- backend/src/controllers/usuarios-controller.js
+++ backend/src/controllers/usuarios-controller.js
@@ -1,5 +1,9 @@
 const db = require('../db/db-create');
 const jsonWebToken = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+
+const saltRounds = 10;
 
 const cadastrarUsuario = async (req, res) => {
 
@@ -11,14 +15,27 @@ const cadastrarUsuario = async (req, res) => {
         })) {
             throw new Error('Usuário já cadastrado.')
         } else {
+
+            const hash = bcrypt.hash(req.body.senha, saltRounds, function(err, hash) {
+                return hash
+            })
+
             const usuario = await db.Usuario.create(req.body)
-            res.json(usuario)
+
+            bcrypt.hash(usuario.senha, saltRounds, async function(err, hash) {
+                await usuario.update({
+                    senha: hash
+                })
+                await usuario.save
+            });
+
+            res.json({mensgem:'Usuario cadastrado com sucesso'})
         }
     } catch (error) {
         console.log(error.message)
-        res.json({error: error.message})
+        res.json({ error: error.message })
     }
-    
+
 };
 
 const login = async (req, res) => {
@@ -29,21 +46,31 @@ const login = async (req, res) => {
     })
 
     try {
-        if(!usuario){
+        if (!usuario) {
             throw new Error('Usuário não encontrado')
-        } 
-        if(usuario.senha === req.body.senha){
-            const token = jsonWebToken.sign(
-                {
-                    usuario: usuario,
-                }, '123'
-            )
-            console.log(token)
-            res.json(token)
         }
+
+        bcrypt.compare(req.body.senha, usuario.senha, function(err, result) {      
+            try {
+                if (result){
+                    const token = jsonWebToken.sign(
+                        {
+                            usuario: usuario,
+                        }, '123'
+                    )
+                    console.log(token)
+                    res.json(token)
+                } else {
+                    throw new Error('Senha Inválida')
+                }
+            } catch (error) {
+                console.log(error.message)
+                res.json({ error: error.message })
+            }            
+        });
     } catch (error) {
         console.log(error.message)
-        res.json({error: error.message})
+        res.json({ error: error.message })
     }
 }
 module.exports = { cadastrarUsuario, login };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
 const app = require('./app.js');
 const db = require('./db/db-create.js')
-require('dotenv').config();
+// require('dotenv').config();
 
 app.listen(3000, async () => {
     try {
         await db.sequelize.authenticate();
-        await db.sequelize.sync({ alter: true });
+        await db.sequelize.sync({ force: true });
         console.log('Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
