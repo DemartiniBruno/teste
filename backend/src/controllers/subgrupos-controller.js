6fb1e4e0766095e70533a9e47b7c6c44410ee2ae
BrunoDemartini <bruno.r.demartini@gmail.com>
2023-09-29 09:45:25 -0300

    ajustando criação de subgrupo

--- backend/src/controllers/subgrupos-controller.js
+++ backend/src/controllers/subgrupos-controller.js
@@ -1,72 +1,48 @@
 const db = require('../db/db-create');
 const jsonWebToken = require('jsonwebtoken');
 
-// const cadastrarSubgrupoAdm = async (req, res) => {
-//     const subgrupo = await db.Subgrupo.create({
-//         nome: req.body.nome,
-//         descricao: req.body.descricao,
-//         grupo_id: req.params.grupo_id
-//     })
-
-//     const usuario = await jsonWebToken.decode(req.headers.authorization, '123')
-
-//     const er_subgrupo = await db.ErUsuarioDoSubgrupo.create({
-//         permissao_adm: true,
-//         subgrupo_id: subgrupo.id,
-//         usuario_id: usuario.usuario.id
-//     })
-
-//     res.json(subgrupo) 
-
-// }
-
-// const cadastrarSubgrupo = async (req, res) => {
-//     const subgrupo = await db.Subgrupo.create({
-//         nome: req.body.nome,
-//         descricao: req.body.descricao,
-//         grupo_id: req.params.grupo_id
-//     })
-
-//     res.json(subgrupo)
-// }
-
-
+//Nessa função vou receber um array com os dados do subgrupo e usar o forEach para criar cada um dos subgrupos
 const cadastrarSubgrupo = async (req, res) => {
-
     try {
-        // const subgrupos_array = req.body
-
-        // subgrupos_array.forEach(async subgrupo => {
-        //     const db_subgrupo = await db.Subgrupo.create(subgrupo)
-        //     console.log(`Subgrupo ${db_subgrupo.nome} - ${db_subgrupo.descricao} criado com sucesso`)
-        // });
         req.body.forEach(async subgrupo => {
+
             const db_subgrupo = await db.Subgrupo.create({
                 nome: subgrupo.nome,
                 descricao: subgrupo.descricao,
                 grupo_id: req.params.grupo_id
+                //Vou definir quais são os parametros do subgrupo individualmente porque puxo o parametro da URL e não só do body do request
+                //Como se trata de vários subgrupos de um único grupo, então o parametro do grupo_id vem pela URL mesmo, não preciso passar pelo body
             })
-            console.log("Criado o subgrupo")
+
+            console.log(`Subgrupo ${subgrupo.nome} - ${subgrupo.descricao} criado com sucesso`)
+
+
+            //Quando o administrador for morador de um dos apartamentos, ao marcar a checkbox de morador deve ser enviado no body a informação: "permissao_adm: true"
+            if(subgrupo.permissao_adm){
+
+                const usuario = await jsonWebToken.decode(req.headers.authorization, '123')
+
+                const er_subgrupo = await db.ErUsuarioDoSubgrupo.create({
+                    subgrupo_id: req.params.subgrupo_id,
+                    usuario_id: usuario.usuario.id,
+                    permissao_adm: true
+                })
+
+                console.log(`Subgrupo ${subgrupo.nome} - ${subgrupo.descricao} criado com sucesso`)
+
+                console.log(`O administrador ${usuario.usuario.nome} é morador do subgrupo ${subgrupo.nome} - ${subgrupo.descricao}`)
+            }
         })
 
         res.json({
-            mensagem: "Criado"
+            mensagem: "Apartamentos criados com sucesso"
+            // ----> ANALISAR QUAL MELHOR RETORNO PARA ESSA SITUAÇÃO <----
         })
 
     } catch (error) {
-        
+        res.json(error.message)
     }
-
-    // const subgrupo = await db.Subgrupo.create({
-    //     nome: req.body.nome,
-    //     descricao: req.body.descricao,
-    //     grupo_id: req.params.grupo_id
-    // })
-    //
-    // res.json(subgrupo)
 }
-
-// implementação
 
 const acessarSubgrupo = async (req, res) => {
     const usuario = await jsonWebToken.decode(req.headers.authorization, '123')
@@ -78,6 +54,8 @@ const acessarSubgrupo = async (req, res) => {
 
     res.json({ mensagem: 'Usuario vinculado ao subgrupo' })
 }
+
+//Funções GET para visualização de informações
 
 const visualizarSubgrupo = async (req, res) => {
     const usuario = await jsonWebToken.decode(req.headers.authorization, '123')
@@ -100,7 +78,6 @@ const visualizarSubgrupo = async (req, res) => {
 }
 
 module.exports = {
-    // cadastrarSubgrupoAdm,
     cadastrarSubgrupo,
     acessarSubgrupo,
     visualizarSubgrupo
