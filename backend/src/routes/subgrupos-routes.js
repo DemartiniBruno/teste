581cad8227d1ba869bf1f47a3c9fcaee8c0ebefb
BrunoDemartini <bruno.r.demartini@gmail.com>
2023-11-20 11:10:39 -0300

    feat: tela principal criada e ligada a API para buscar os subgrupos

--- backend/src/routes/subgrupos-routes.js
+++ backend/src/routes/subgrupos-routes.js
@@ -6,7 +6,7 @@ const subgruposRouters = Router();
 // subgruposRouters.post('/:grupo_id/subgruposadm', controller.cadastrarSubgrupoAdm)
 subgruposRouters.post('/:grupo_id/subgrupos/', controller.cadastrarSubgrupo)
 subgruposRouters.post('/acessar/', controller.acessarSubgrupo)
-subgruposRouters.get('/:grupo_id/subgrupos/:id', controller.visualizarSubgrupo)
+// subgruposRouters.get('/:grupo_id/subgrupos/:id', controller.visualizarSubgrupo)
 subgruposRouters.put('/:grupo_id/subgrupo/:subgrupo_id', controller.editarSubgrupo)
 // subgruposRouters.get('/subgrupos', controller.visualizarTodosSubgrupo)
 
---
+++ frontend/src/script/tela_principal.js
@@ -0,0 +1,53 @@
+const get_subgrupo = async () => {
+    try {
+
+        const teste = await fetch('http://127.0.0.1:3000/grupos/1/subgrupos/', {
+            headers: {
+                // "Content-Type": "application/json",
+                "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3VhcmlvIjp7ImlkIjoxLCJub21lIjoiYnJ1bm8iLCJlbWFpbCI6ImJydW5vQHRlc3RlLmNvbSIsInNlbmhhIjoiJDJiJDEwJGV6a1Q3TjZDUG9JL3RTdC4zQ3NLSS4xdGhPZ1dYNVJweldEUjBSRXFjbjFvWUdRTlBIWHBDIiwiY3JlYXRlZEF0IjoiMjAyMy0wOS0yNVQxODo0NDowMC44MDJaIiwidXBkYXRlZEF0IjoiMjAyMy0wOS0yNVQxODo0NDowMC44ODZaIiwiZGVsZXRlZEF0IjpudWxsfSwiaWF0IjoxNjk1NzI4MzA5fQ.S-5N1B5eOGdchcki10DdmJGLYwvAMK0ILzhKeMHz9C0"
+            }
+
+        })
+            .then(async function (dados) {
+
+                return dados.json()
+            })
+            .then(async function (dados_convertidos) {
+                return dados_convertidos[0].subgrupos
+            })
+
+        teste.forEach(element => {
+            const bloco = document.createElement("div")
+            bloco.setAttribute("class","bloco_apartamento")
+            const apartamento = document.createElement("h2")
+            // apartamento.setAttribute("value", element.nome)
+            apartamento.innerText = element.nome
+            const descricao = document.createElement("p")
+            descricao.innerText = element.descricao
+            
+            const btn_abrir = document.createElement("input")
+            btn_abrir.setAttribute("type", "button")
+            btn_abrir.value = "abrir"
+            btn_abrir.disabled=true
+
+            console.log(element.er_usuario_do_subgrupos.length)
+
+            if(element.er_usuario_do_subgrupos!=0){
+               btn_abrir.disabled=false   
+            }
+
+            
+            bloco.appendChild(apartamento) 
+            bloco.appendChild(descricao)
+            bloco.appendChild(btn_abrir)
+
+            
+
+            document.getElementById("apartamentos").appendChild(bloco)
+
+        });
+
+    } catch (error) {
+        alert(error.message)
+    }
+}
