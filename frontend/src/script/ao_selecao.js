0530f67959986341b1682eacb6c5d24d092f789b
Bruno Demartini <bruno.r.demartini@gmail.com>
2023-12-05 23:47:07 -0300

    adicionado validação de relação na tela de seleção

--- frontend/src/script/ao_selecao.js
+++ frontend/src/script/ao_selecao.js
@@ -1,3 +1,30 @@
+const validar_relacao = async () => {
+  const relacao = await fetch(`http://127.0.0.1:3000/relacao`, {
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": localStorage.token
+            }
+        })
+            .then(async function (dados) {
+
+                return dados.json()
+            })
+            .then(async function (dados_convertidos) {
+                return dados_convertidos
+            })
+
+        if (relacao) {
+            if (relacao.permissao_adm) {
+                
+              location.href = './principal_adm.html'
+
+            } else {
+                // REDIRECIONAR PARA TELA DE SELEÇÃO
+                location.href = './subgrupo.html'
+            }
+        }
+}
+
 const acessar_subgrupo = async () => {
     const codigo_subgrupo = {
         codigo_acesso: document.getElementById('input_entrar').value
